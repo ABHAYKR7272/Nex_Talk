c63df930785d16ced11004e21885062d0cbc17e8
ABHAY KUMAR NISHAD <abhaykrnshd2276@gmail.com>
2026-05-22 15:04:44 +0530

    Update STUN and TURN servers in authRoutes.js
    Removed unused STUN and TURN servers, added Cloudflare STUN server.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -150,10 +150,7 @@ router.get('/turn-credentials', protect, async (req, res) => {
           { urls: 'stun:stun.l.google.com:19302' },
           { urls: 'stun:stun1.l.google.com:19302' },
           { urls: 'stun:stun2.l.google.com:19302' },
-          { urls: 'stun:stun3.l.google.com:19302' },
-          { urls: 'turn:openrelay.metered.ca:80', username: 'openrelayproject', credential: 'openrelayproject' },
-          { urls: 'turn:openrelay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayproject' },
-          { urls: 'turn:openrelay.metered.ca:443?transport=tcp', username: 'openrelayproject', credential: 'openrelayproject' },
+          { urls: 'stun:stun.cloudflare.com:3478' },
           { urls: 'stun:stun2.l.google.com:19302' },
           { urls: 'stun:stun.cloudflare.com:3478' },
         ]
